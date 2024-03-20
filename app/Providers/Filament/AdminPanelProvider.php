9078705155f8fbba32c1f90881c21f0407b0442f
kodrat__ <kodratcoc@gmail.com>
2024-03-20 10:21:52 +0700

    improvement sidebar background

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,7 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->sidebarCollapsibleOnDesktop()
             ->font('Poppins')
-            ->brandName('App')
+            ->brandName('E Perpustakaan')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
